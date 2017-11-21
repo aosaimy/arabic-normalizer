04fff37de7a7eb80a5cb3e354108cd29ac7a1388
Abdulrahman Alosaimy <a.osaimy@gmail.com>
2017-11-21 19:47:46 +0000

    v0.5

---
+++ main.js
@@ -0,0 +1,248 @@
+#!/usr/bin/env node
+var fs = require('fs');
+var es = require('event-stream');
+
+utf8_to_ascii = {'\u0660':'0', '\u0661':'1', '\u0662':'2',
+                '\u0663':'3', '\u0664':'4', '\u0665':'5',
+                '\u0666':'6', '\u0667':'7', '\u0668':'8',
+                '\u0669':'9', '\u06F0':'0', '\u06F1':'1',
+                '\u06F2':'2', '\u06F3':'3', '\u06F4':'4',
+                '\u06F5':'5', '\u06F6':'6', '\u06F7':'7',
+                '\u06F8':'8', '\u06F9':'9'}
+
+// Patterns
+// Match any character not in the Arabic charts
+// Including presentation forms
+p_not_arb = /[^\u0600-\u06FF\u0750-\u077F\uFB50-\uFDFF\uFE70-\uFEFF]/g
+
+// Segment punctuation
+latin_punc = /([\u0021-\u002F\u003A-\u0040\u005B-\u0060\u007B-\u007E\u00A1-\u00BF\u2010-\u2027\u2030-\u205E\u20A0-\u20B5\u2E2E]{1})/g
+arb_punc = /([\u0609-\u060D\u061B-\u061F\u066A\u066C-\u066D\u06D4]{1})/g
+other_punc = /([\u2E2E]{1})/g
+
+// Segment digits
+ascii_digit = /(\d+)/g
+arb_digit = /([\u06F0-\u06F9\u0660-\u0669]+)/g
+arb_letter = /[\u0627-\u063A\u0641-\u064A]/
+
+// Patterns commonly found in newswire (e.g., the ATB)
+date_cluster = /\d+\-\d+/g
+email_cluster = /[\w\-][\w\-\.]+@[\w\-][\w\-\.]+[a-zA-Z]{1,4}/g
+url_cluster = /[a-zA-Z]+.[a-zA-Z]+/g
+ellipsis = /(\.{2,})/g
+
+// Strip diacritics, tatweel, extended Arabic
+p_diac = /[\u064B-\u0652]/g
+p_diac_full = /^[\u064B-\u0652]+$/g
+p_tat = /ـ/g
+p_quran = /[\u0615-\u061A\u06D6-\u06E5]/g
+
+// Normalize alif
+p_alef = /ا|إ|أ|آ|\u0671/
+
+// Escape Basic Latin (U0000) and Latin-1 (U0080) characters
+p_latin = /[\u0000-\u00FF]/
+
+function arb_digit_to_ascii(string){
+    var ascii_digits = []
+    for (var c_idx in string) {
+        new_digit = utf8_to_ascii[string[c_idx]]
+        if (new_digit)
+            ascii_digits.push(new_digit)
+        else
+            return string
+    }
+    return '' + ascii_digits
+}
+
+function normalize_diac(string){
+    return string
+            // to delete starting taskeels
+            .replace(/(?=\s|^)[\u064B-\u0652]*/g,'')
+            //beggining of surah
+            .replace(/([لمحكهعص])ٓ/g,"$1")
+
+            //remove words that has only diacritics
+            .replace(/ [\u064B-\u0652]*(?=\s)/g,"")// used lookahead: look ahead if there is a space
+
+            // //delete harakah before the mad letter (e.g. fathah then alif)
+            // .replace(/َا/g,"ا")
+            // .replace(/ِي/g,"ي")
+            // .replace(/ُو/g,"و")
+
+            // shaddah before harakah
+            .replace(/([ًٌٍَُِ])ّ/g,"ّ$1")
+
+            // add sukon to mad Alif
+            .replace(/َا/g,"َاْ")
+            .replace(/ِي/g,"ِيْ")
+            .replace(/ُو/g,"ُوْ")
+
+            //remove diac if the same is put more than once
+            .replace(/([\u064B-\u0652])\1/g,"$1")
+
+            // tanween then end
+            .replace(/اً(?=\s|$)/g,"ًا")
+            .replace(/ىً(?=\s|$)/g,"ًى")
+
+            
+            //bottom hamza
+            .replace(/إ[ًٌٍَُِ]/g,"إ")
+            
+            //remove incompaitble diacs
+            .replace(/([ًٌٍَُِْ])[ًٌٍَُِْ]/g,"$1")
+
+            //remove beginning shaddah
+            .replace(/(?=\s|^)ّ/g,"")
+
+            //Tanween in wrong place
+            .replace(/[ًٌٍ]([^ ][^ ])/g,"$1")
+
+            //bottom hamza
+            .replace(/إ[ًٌٍَُ]/g,"إِ")
+            .replace(/إ([^ِ])/g,"إِ$1")
+}
+
+function carefully_segment(line,config){
+    var tokens = []
+    var stripped = line.trim()
+    if ( stripped.length > 0 && stripped[0] == '<'){
+        tokens.push(line)
+        return tokens
+    }
+    
+    line = line.replace(/\u00AB/g,' " ')
+    line = line.replace(/\u00BB/g,' " ')
+    line = line.replace(/\u061F/g,' ? ')
+    line = line.replace(/\u2E2E/g,' ? ')
+    line = line.replace(/&lt;/g,' < ')
+    line = line.replace(/&gt;/g,' > ')
+    line = line.replace(/&amp;/g,' & ')
+    line = line.replace(/&quot;/g,' " ')
+    var splitted  =line.split(" ")
+    for (let word of splitted){
+        if ( date_cluster.test(word))
+            tokens.push(word)
+        else if ( email_cluster.test(word))
+            tokens.push(word)
+        else if ( url_cluster.test(word))
+            tokens.push(word)   
+        else if ( /(-RRB-)|(-LRB-)/.test(word))
+            tokens.push(word)   
+        else if ( ellipsis.test(word))
+            tokens = tokens.concat(word.replace(ellipsis, ' $1 ').split(" "))
+        else{
+            if(config.remove_punc){
+                word = word.replace(latin_punc, '')
+                word = word.replace(other_punc, '')
+                word = word.replace(arb_punc, '')
+            }
+            else if(config.punc){
+                word = word.replace(latin_punc, ' $1 ')
+                word = word.replace(other_punc, ' $1 ')
+                word = word.replace(arb_punc, ' $1 ')
+            }
+            if(config.s_digit){
+                word = word.replace(ascii_digit, ' $1 ')
+                word = word.replace(arb_digit, ' $1 ')
+            }
+            word = word.replace(/([\(\)\+])/g,' $1 ').split(/ +/)
+            word.forEach(v=>{
+                // to prevent adding only taskeel words
+                if ( !p_diac_full.test(v))
+                    tokens.push(v)
+            })
+        }
+    }
+    return tokens
+}
+
+var norm_input_line = function (line,config){
+    if(config.normalize_diac)
+        line = normalize_diac(line)
+    line = carefully_segment(line,config)
+    new_line = []
+    for (let word of line) {
+        // if(config.normalize_diac && normalize_diac(word) != word)
+        //     console.log("n=",normalize_diac(word)," o=",word)
+        if (p_latin.test(word))
+            new_line.push(word.trim())
+        else if (arb_digit.test(word)){
+            asciified = arb_digit_to_ascii(word)
+            new_line.push(asciified)
+        }
+        else if (word == '\u060C')
+            new_line.push(',')
+        else if (word == '\u061F' || word == '\u2E2E')
+            new_line.push('?')
+        else{
+            if(config.diac)
+                word = word.replace(p_diac,'')
+            if(config.tatweel)
+                word = word.replace(p_tat,'')
+            if(config.alif)
+                word = word.replace(p_alef,'ا')
+            word = word.replace(p_quran,'')
+            if(word!='')
+                new_line.push(word.trim())
+        }
+    }
+    new_line = new_line.join(" ")
+    return new_line
+}
+var default_config = {
+    'diac':false,
+    'alif':false,
+    'tatweel':true,
+    'digits':true,
+    'punc':true,
+    's_digits':true,
+    'remove_punc': false,
+    'debug': false,
+    'normalize_diac': true
+}
+
+if (require.main === module) { // called directly
+    var argv = require('yargs')
+    .usage('Usage: $0 [-m all|exceptPos] [-st] [-e example_filename] -f filename')
+    .default('f',"/dev/stdin")
+    .describe('f','input file')
+    .boolean('diac').describe('diac','remove diac').default('diac',false)
+    .boolean('alif').describe('alif','normalize alif').default('alif',false)
+    .boolean('tatweel').describe('tatweel','remove tatweel').default('tatweel',true)
+    .boolean('digits').describe('digits','romanize digits').default('digits',true)
+    .boolean('punc').describe('punc','segment punctuations').default('punc',true)
+    .boolean('s_digits').describe('s_digits','segment digits').default('s_digits',true)
+    .boolean('normalize_diac').describe('normalize_diac','normalize diacritics to one standard').default('normalize_diac',true)
+    .boolean('remove_punc').describe('remove_punc','remove punctuations').default('remove_punc',false)
+    .help('h')
+    .alias('h', 'help')
+    .argv
+    
+    var lineId = 0
+    fs.createReadStream(argv.f)
+       .pipe(es.split("\n"))
+       .pipe(es.through(function(line){
+            lineId = lineId + 1
+            this.emit('data',norm_input_line(line,argv)+"\n")
+       },function(){
+        console.error(`Processed ${lineId} lines`)
+       }))
+      .pipe(process.stdout)
+}
+else
+    module.exports = function (infile,config){
+        
+        config = config || {}
+        Object.keys(default_config).forEach(e=>config[e] = config[e] || default_config[e])
+
+        if(!Array.isArray(infile))
+            infile = infile.split("\n")
+        var text = []
+        for (let line of infile){
+            text.push(norm_input_line(line,config))
+        }
+        if(config.debug)
+        console.error(`Processed ${text.length} lines`)
+        return text.join("\n")
+    }
---
+++ test/diacritics.js
@@ -0,0 +1,55 @@
+// using assert passed to the test function that just logs failures 
+var assert = require('assert');
+var normalizer = require("../main.js");
+var buckwalter = require("../../buckwalter.js");
+describe('diacritics', ()=>{
+	describe('remove_diacritics', function() {
+		it('should remove diacs', ()=>{
+			assert.equal(u2b(normalizer(b2u(">alif"),{diac:true})), ">lf")
+		})
+		it('should not removed diacs', ()=>{
+			assert.equal(u2b(normalizer(b2u(">alif"),{diac:false})), ">alif")
+		})
+	})
+
+	describe('normalize_diacritics', function() {
+		it("should remove starting diacritics", ()=>{
+			assert.equal(u2b(normalizer(b2u("aamno"),{diac:false,normalize_diac:true})), "mno")
+		})
+		it("should remove words that has only diacritics", ()=>{
+			assert.equal(u2b(normalizer(b2u("bsm i min"),{diac:false,normalize_diac:true})), "bsm min")
+		})
+		it("should add sokun diacritic on the vowel letters (e.g. fathah then alif)", ()=>{
+			assert.equal(u2b(normalizer(b2u("laA"),{diac:false,normalize_diac:true})), "laAo")	
+		})
+		it("should remove duplicates of the same diacritic", ()=>{
+			assert.equal(u2b(normalizer(b2u("maano"),{diac:false,normalize_diac:true})), "mano")
+		})
+		it("should alif tanween be tanween then the alif", ()=>{
+			assert.equal(u2b(normalizer(b2u("mtmyzAF"),{diac:false,normalize_diac:true})), "mtmyzFA")
+		})
+		it("should swap shaddah if before harakah", ()=>{
+			assert.equal(u2b(normalizer(b2u("mima~A"),{diac:false,normalize_diac:true})), "mim~aAo")
+		})
+		it("should remove incompatible diacritics", ()=>{
+		assert.equal(u2b(normalizer(b2u("miano"),{diac:false,normalize_diac:true})), "mino")
+		})
+		it("should remove tanween in wrong place", ()=>{
+			assert.equal(u2b(normalizer(b2u("mFno"),{diac:false,normalize_diac:true})), "mno")
+			assert.equal(u2b(normalizer(b2u("sma~A"),{diac:false,normalize_diac:true})), "sm~aAo")
+		})
+		it("should remove shaddah at the beginning of word", ()=>{
+			assert.equal(u2b(normalizer(b2u("~mino"),{diac:false,normalize_diac:true})), "mino")
+		})
+		it("should normalize bottom hamza", ()=>{
+			assert.equal(u2b(normalizer(b2u("<ansAn"),{diac:false,normalize_diac:true})), "<insAn")
+			assert.equal(u2b(normalizer(b2u("<nsAn"),{diac:false,normalize_diac:true})), "<insAn")
+		})
+	})
+})
+function b2u(text){
+	return buckwalter.bw2utf(text)
+}
+function u2b(text){
+	return buckwalter.utf2bw(text)
+}
